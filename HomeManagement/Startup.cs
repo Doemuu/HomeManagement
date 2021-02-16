81cc1679592e5f451ef64731a01fc1870a7a6bb4
Doemuu <doemu@outlook.com>
2021-02-16 11:05:30 +0100

    Implement appsettings, jwtbearer service

--- HomeManagement/Startup.cs
+++ HomeManagement/Startup.cs
@@ -1,6 +1,8 @@
 using HomeManagement.Connector;
 using HomeManagement.Connector.Database;
+using HomeManagement.Entities;
 using HomeManagement.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -9,10 +11,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace HomeManagement
@@ -29,6 +33,33 @@ namespace HomeManagement
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // configure strongly typed settings objects
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            // configure jwt authentication
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    // set clockskew to zero so tokens expire exactly at token expiration time (instead of 5 minutes later)
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
             services.AddControllers();
 
             services.AddTransient<IDatabaseConnector, DatabaseConnector>();
@@ -36,6 +67,10 @@ namespace HomeManagement
             services.AddTransient<ShoppingService>();
 
             services.AddTransient<NotesService>();
+
+            services.AddTransient<UserService>();
+
+            services.AddTransient<AuthService>();
 
             services.AddSwaggerGen(swagger =>
             {
@@ -61,6 +96,9 @@ namespace HomeManagement
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "HomeManagementAPI");
             });
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseHttpsRedirection();
 
